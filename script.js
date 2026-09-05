1dd3d51baa6e0e16a93b40b3e89016ed49112334
Mikiyas Gashaw <mikiyasgashaw121@outlook.com>
2026-09-05 22:57:53 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
 const chat = document.getElementById("chat");
 const promptInput = document.getElementById("prompt");
 const sendBtn = document.getElementById("sendBtn");
-const clear = document.getElementById("clear");
-const headerText = document.getElementById("headerText");
-const title = document.getElementById("title");
-const chatTime = document.querySelector(".chat-time");
-const clearConversetion = document.getElementById("clearConversetion");
-const welcamText = document.querySelector(".welcamText");
-const newBtn = document.querySelector(".new-chat");
-const chatItem = document.querySelector(".chat-item");
 
-const date = new Date();
 const API_URL =
   "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions";
 const API_KEY = "Add your api key here please";
 
 let conversation = [];
-// let history = [];
 
 sendBtn.addEventListener("click", sendMessage);
-promptInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    sendMessage();
-  }
-});
 
 async function sendMessage() {
-  let time = date.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  });
-
-  // hero section
-  welcamText.classList.add("none");
-
   // Get the user's message
 
   const prompt = promptInput.value.trim();
@@ -50,10 +25,7 @@ async function sendMessage() {
 
   chat.innerHTML += `
         <div class="message user">
-          <div class="bubble">
-            <p>${prompt}</p>
-            <span class="message-time">${time}</span>
-          </div>  
+            ${prompt}
         </div>
     `;
 
@@ -64,17 +36,6 @@ async function sendMessage() {
     content: prompt,
   });
 
-  // TO ADD header name
-
-  headerText.innerHTML = conversation[0]["content"];
-
-  // ADD title name to sidebar name
-
-  title.innerHTML = conversation[0]["content"];
-
-  // ADD Time to the title
-
-  chatTime.innerHTML = time;
   // Clear input
 
   promptInput.value = "";
@@ -83,12 +44,7 @@ async function sendMessage() {
 
   chat.innerHTML += `
         <div id="loading" class="message ai">
-            <div class="dots">
-            Thinking
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
+            Thinking...
         </div>
     `;
 
@@ -103,7 +59,7 @@ async function sendMessage() {
       },
 
       body: JSON.stringify({
-        model: "google/gemma-4-31B-it",
+        model: "gemini-3.7-flash",
         messages: conversation,
       }),
     });
@@ -111,7 +67,6 @@ async function sendMessage() {
     const data = await response.json();
 
     const aiMessage = data.choices[0].message.content;
-    console.log(aiMessage);
 
     // Remove loading
 
@@ -122,12 +77,8 @@ async function sendMessage() {
     const formattedMessage = marked.parse(aiMessage);
 
     chat.innerHTML += `
-            <div class="message">
-              <div class="avater">🤖</div>
-              <div class="bubble">
-                <p>${formattedMessage}</p>
-                <span class="message-time"> 10:32 AM </span>
-              </div>  
+            <div class="message ai">
+                ${formattedMessage}
             </div>
         `;
 
@@ -157,26 +108,3 @@ async function sendMessage() {
         `;
   }
 }
-
-// DARK MODE
-const darkMode = document.getElementById("svg");
-darkMode.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-});
-
-// crear the date
-clear.addEventListener("click", () => {
-  chat.innerHTML = "";
-  headerText.innerHTML = "";
-  // hero section
-  welcamText.classList.remove("none");
-});
-
-clearConversetion.addEventListener("click", () => {
-  title.innerHTML = "";
-  chatTime.innerHTML = "";
-  chat.innerHTML = "";
-  headerText.innerHTML = "";
-  // hero section
-  welcamText.classList.remove("none");
-});
